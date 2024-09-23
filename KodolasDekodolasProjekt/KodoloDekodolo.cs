da03d8451cac5948e082b9a28efe06c047b63b79
gycsaba <gycsaba@SZT9-24>
2024-09-23 14:59:29 +0200

    KodolDekodol osztály

---
+++ KodolasDekodolasProjekt/KodoloDekodolo.cs
@@ -0,0 +1,12 @@
+﻿namespace KodolasDekodolasProjekt
+{
+    public class KodoloDekodolo
+    {
+        private bool _kodolas; // ha nem kódolás akkor dekódolás
+        private int _oszlopokSzama;
+        private string kulcs;
+
+        public string Eredmeny => string.Empty;
+
+    }
+}
--- KodolasDekodolasProjekt/Program.cs
+++ KodolasDekodolasProjekt/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ide írjon dekódolási és kódolási eredményeket
